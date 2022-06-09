05b1e0d9d57c6991002cd061b0ec7862f1f15968
21ciprian <2021ciprianoffice@gmail.com>
2022-06-10 00:03:04 +0100

    Styled MainArticle component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,9 +4,11 @@ import SecondaryArticle from '../SecondaryArticle/SecondaryArticle'
 import styles from './Main.module.css'
 
 function Main({articles}) {
+	console.log('all articles: ', articles)
 	return (
 		<main className={styles.main}>
 			<MainArticle article={articles?.[0]} />
+			<hr />
 			{articles?.slice(1).map(article => (
 				<SecondaryArticle article={article} key={article.url} />
 			))}
--- src/components/MainArticle/MainArticle.jsx
+++ src/components/MainArticle/MainArticle.jsx
@@ -2,24 +2,35 @@ import React from 'react'
 import styles from './MainArticle.module.css'
 
 function MainArticle({article}) {
+	const published = article.publishedAt.split(/T/)
+	const date = published[0]
+	const time = published[1].slice(0, -1)
+
+	console.log({article})
+	console.log({date})
+	console.log({time})
 	return (
 		<article className={styles.mainArticle}>
-			<img src={article.image} alt={article.title} />
-			<h2>{article.title}</h2>
-			<p>{article.description}</p>
-			<p>
-				{article.content.substring(0, 200)}...
-				<a href={article.url} target='_blank' rel='noreferrer'>
-					<span>Read full article</span>
-				</a>
-			</p>
+			<img src={article.image} alt={article.title} className={styles.mainImg} />
+			<section className={styles.mainDesc}>
+				<h2 className={styles.mainDescTitle}>{article.title}</h2>
+				<p className={styles.mainDescContent}>
+					{article.content.substring(0, 200)}...{' '}
+					<a href={article.url} target='_blank' rel='noreferrer'>
+						<span className={styles.fullArticleLink}>Read full article</span>
+					</a>
+				</p>
 
-			<p>
-				Source:{' '}
-				<a href={article.source.url} target='_blank' rel='noreferrer'>
-					{article.source.name}
-				</a>{' '}
-			</p>
+				<p className={styles.articleSource}>
+					<span className={styles.source}>
+						<a href={article.source.url} target='_blank' rel='noreferrer'>
+							{article.source.name}
+						</a>
+					</span>{' '}
+					<span className={styles.source}>{date}</span>{' '}
+					<span className={styles.source}>{time}</span>
+				</p>
+			</section>
 		</article>
 	)
 }
