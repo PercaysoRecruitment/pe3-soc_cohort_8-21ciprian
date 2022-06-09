eb179dbc932ee65e493378fd38fa5efb34adfd35
21ciprian <2021ciprianoffice@gmail.com>
2022-06-09 22:38:03 +0100

    Created MainArticle and SecondaryArticle components

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,28 +1,14 @@
 import React from 'react'
+import MainArticle from '../MainArticle/MainArticle'
+import SecondaryArticle from '../SecondaryArticle/SecondaryArticle'
+import styles from './Main.module.css'
 
 function Main({articles}) {
 	return (
-		<main>
-			{articles?.map(article => (
-				<article key={article.url}>
-					<h2>{article.title}</h2>
-					<p>{article.description}</p>
-					<p>
-						{article.content.substring(0, 200)}...
-						<a href={article.url} target='_blank' rel='noreferrer'>
-							<span>Read full article</span>
-						</a>
-					</p>
-
-					<p>
-						Source:{' '}
-						<a href={article.source.url} target='_blank' rel='noreferrer'>
-							{article.source.name}
-						</a>{' '}
-					</p>
-
-					<img src={article.image} alt={article.title} />
-				</article>
+		<main className={styles.main}>
+			<MainArticle article={articles?.[0]} />
+			{articles?.slice(1).map(article => (
+				<SecondaryArticle article={article} key={article.url} />
 			))}
 		</main>
 	)
---
+++ src/components/SecondaryArticle/SecondaryArticle.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function SecondaryArticle({article}) {
+	return (
+		<article>
+			<h2>{article.title}</h2>
+			<p>{article.description}</p>
+			<p>
+				{article.content.substring(0, 200)}...
+				<a href={article.url} target='_blank' rel='noreferrer'>
+					<span>Read full article</span>
+				</a>
+			</p>
+
+			<p>
+				Source:{' '}
+				<a href={article.source.url} target='_blank' rel='noreferrer'>
+					{article.source.name}
+				</a>{' '}
+			</p>
+
+			<img src={article.image} alt={article.title} />
+		</article>
+	)
+}
+
+export default SecondaryArticle
