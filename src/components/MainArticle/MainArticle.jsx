d86f726f5b1cf530c0225f8a6e949b7b68956328
21ciprian <2021ciprianoffice@gmail.com>
2022-06-13 13:14:45 +0100

    Added tests to check if all the components are rendered in MainArticle component

--- src/components/MainArticle/MainArticle.jsx
+++ src/components/MainArticle/MainArticle.jsx
@@ -9,21 +9,24 @@ function MainArticle({article}) {
 		<article data-testid='maTestId' className={styles.mainArticle}>
 			<div className={styles.imgContainer}>
 				<img
+					data-testid='imgTestId'
 					src={article?.image}
 					alt={article?.title}
 					className={styles.mainImg}
 				/>
 			</div>
 			<section className={styles.mainDesc}>
-				<h2 className={styles.mainDescTitle}>{article?.title}</h2>
-				<p className={styles.mainDescContent}>
+				<h2 data-testid='titleTestId' className={styles.mainDescTitle}>
+					{article?.title}
+				</h2>
+				<p data-testid='descTestId' className={styles.mainDescContent}>
 					{article?.content?.substring(0, article?.content?.length - 15)}...{' '}
 					<a href={article?.url} target='_blank' rel='noreferrer'>
 						<span className={styles.fullArticleLink}>Read full article</span>
 					</a>
 				</p>
 
-				<p className={styles.articleSource}>
+				<p data-testid='srcTestId' className={styles.articleSource}>
 					<span className={styles.source}>
 						<a href={article?.source?.url} target='_blank' rel='noreferrer'>
 							{article?.source?.name}
--- src/components/MainArticle/MainArticle.test.js
+++ src/components/MainArticle/MainArticle.test.js
@@ -1,9 +1,33 @@
 import { render, screen } from '@testing-library/react'
 // import userEvent from '@testing-library/user-event'
 import MainArticle from './MainArticle'
-test.only('Should render MainArticle component', async () => {
+test('Should render MainArticle component', async () => {
   render(<MainArticle />)
   const article = await screen.findByTestId('maTestId')
   expect(article).toBeInTheDocument()
+  // screen.debug()
+})
+test('Should render image in MainArticle component', async () => {
+  render(<MainArticle />)
+  const image = await screen.findByTestId('imgTestId')
+  expect(image).toBeInTheDocument()
+  // screen.debug()
+})
+test('Should render title in MainArticle component', async () => {
+  render(<MainArticle />)
+  const title = await screen.findByTestId('titleTestId')
+  expect(title).toBeInTheDocument()
+  // screen.debug()
+})
+test('Should render description in MainArticle component', async () => {
+  render(<MainArticle />)
+  const desc = await screen.findByTestId('descTestId')
+  expect(desc).toBeInTheDocument()
+  // screen.debug()
+})
+test('Should render article source in MainArticle component', async () => {
+  render(<MainArticle />)
+  const src = await screen.findByTestId('srcTestId')
+  expect(src).toBeInTheDocument()
   // screen.debug()
 })
