26955afc598924f62fc98091dd18fed3eec7eaf1
21ciprian <2021ciprianoffice@gmail.com>
2022-06-13 13:21:18 +0100

    Added tests to check if all the components are rendered in SecondaryArticle component

--- src/components/SecondaryArticle/SecondaryArticle.jsx
+++ src/components/SecondaryArticle/SecondaryArticle.jsx
@@ -1,33 +1,36 @@
 import styles from './SecondaryArticle.module.css'
 
 function SecondaryArticle({article}) {
-	const published = article.publishedAt.split(/T/)
-	const date = published[0]
-	const time = published[1].slice(0, -1)
+	const published = article?.publishedAt?.split(/T/)
+	const date = published?.[0]
+	const time = published?.[1].slice(0, -1)
 	return (
-		<article className={styles.secondary}>
+		<article data-testid='saTestId' className={styles.secondary}>
 			<div className={styles.secondaryContainer}>
 				<div className={styles.imgContainer}>
 					{' '}
 					<img
-						src={article.image}
-						alt={article.title}
+						data-testid='saImgTestId'
+						src={article?.image}
+						alt={article?.title}
 						className={styles.secondaryImg}
 					/>
 				</div>
 				<section className={styles.secondaryDesc}>
-					<h2 className={styles.secondaryDescTitle}>
-						<a href={article.url} target='_blank' rel='noreferrer'>
-							{article.title}
+					<h2 data-testid='saTitleTestId' className={styles.secondaryDescTitle}>
+						<a href={article?.url} target='_blank' rel='noreferrer'>
+							{article?.title}
 						</a>
 					</h2>
-					<p className={styles.secondaryDescContent}>
-						{article.content.substring(0, 150)}...{' '}
+					<p
+						data-testid='saDesceTestId'
+						className={styles.secondaryDescContent}>
+						{article?.content?.substring(0, 150)}...{' '}
 						<span className={styles.fullArticleLink}>Read full article</span>
 					</p>
 
-					<p className={styles.articleSource}>
-						<span className={styles.source}>{article.source.name}</span>{' '}
+					<p data-testid='saSrcTestId' className={styles.articleSource}>
+						<span className={styles.source}>{article?.source?.name}</span>{' '}
 						<span className={styles.source}>{date}</span>{' '}
 						<span className={styles.source}>{time}</span>
 					</p>
