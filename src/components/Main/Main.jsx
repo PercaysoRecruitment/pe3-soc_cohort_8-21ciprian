32b205cc7499dc2c7a0a20171869a7814543e96b
21ciprian <2021ciprianoffice@gmail.com>
2022-06-13 14:19:22 +0100

    Added comments to Main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,8 +5,11 @@ import styles from './Main.module.css'
 function Main({articles}) {
 	return (
 		<main data-testid='mainTestId' className={styles.main}>
+			{/* populate MainArticle component with the first object from the fetched data */}
+
 			<MainArticle article={articles?.[0]} />
 			<hr />
+			{/* populate SecondaryArticle component with all but the first object from the fetched data */}
 			<section
 				data-testid='mainSectionTestId'
 				className={styles.secondarySection}>
